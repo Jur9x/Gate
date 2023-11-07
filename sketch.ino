99a707d6db6f08a7e7e7e93f0028a36a80fa45ac
Jur9x <60826133+Jur9x@users.noreply.github.com>
2023-11-07 23:24:59 +0100

    update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,25 +1,32 @@
+//current state
 #include <IRremote.h>
 
-const byte IR_RECEIVE_PIN = 12;
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
-void setup()
-{
-   Serial.begin(9600);
-   Serial.println("IR receive test");
-   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);// onboard LED blinks with received IR signal
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
-void loop()
-{
-   if (IrReceiver.decode())
-   {
-      unsigned long keycode = IrReceiver.decodedIRData.command;
-      Serial.println(keycode, HEX);
-      if ((IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT)) // ignore repeat code
-      {
-         IrReceiver.resume();
-         return;
-      }
-      IrReceiver.resume();
-   }
+void loop(){
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, DEC);
+        irrecv.resume();
+  }
+  if(digitalRead(9) == HIGH){
+    digitalWrite(2, HIGH);
+  }
+   if(digitalRead(10) == HIGH){
+    digitalWrite(2, LOW);
+  }
 }
