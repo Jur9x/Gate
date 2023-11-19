3cb8568ecf3f9262e37aef6a38f1d48c648a162d
Jur9x <60826133+Jur9x@users.noreply.github.com>
2023-11-19 23:17:58 +0100

    update sketch.ino 19.11.2023

--- sketch.ino
+++ sketch.ino
@@ -5,7 +5,13 @@ const int RECV_PIN = 12;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-int a,b,c;
+int spinacZavreno = 10;
+int spinacOtevreno = 11;
+
+int stavOtevreSe = 0;
+int stavZavreSe = 1;
+
+byte speed = 255;
 
 unsigned long kodStartu = 3389572121;
 unsigned long kodStopu = 3439970883;
@@ -16,34 +22,40 @@ void setup(){
   irrecv.blink13(true);
   pinMode(10, INPUT);//spinca
   pinMode(11, INPUT);//spinac
-  pinMode(2, OUTPUT);//dioda
-  pinMode(9, OUTPUT);//IR senzor vzdálenosti
+  pinMode(2, OUTPUT);//motor
+  pinMode(3, OUTPUT);//motor
+  pinMode(8, INPUT);//IR senzor
 }
 
-void setupIRSenzoruVzdalenosti(){
-  digitalWrite(9, HIGH);
-  delay(1);
-  a=analogRead(A3);
-  digitalWrite(9, LOW);
-  delay(1);
-  b=analogRead(A3);
-  c=a-b;
-
-  Serial.print(a);//signál se zapnutou ledkou na senzoru
-  Serial.print(b);//signál bez zapnuté ledky na senzoru
-  Serial.print(c);//signál bez okolního rušení
-  //c bude výsledek který budu chtít na if podmínku
-}
-
-void startMotor(){
-  digitalWrite(2, HIGH);//simulace zapnuti motoru
+void startMotor(){ //nevim na jakou stranu se jak toci//
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  /*if(stavOtevreSe = 1){   ^^^^ kod na test h mustku
+    analogWrite(5, speed); real kod
+    analogWrite(6, 0);
+  }
+  if(stavZavreSe = 1){
+    analogWrite(6, speed);
+    analogWrite(5, 0);
+  }*/
 }
 
 void stopMotor(){
-  digitalWrite(2, LOW);//simulace vypnuti motoru
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
 }
 
 void loop(){
+  if(spinacZavreno == HIGH){
+    //prednastavi stav brany
+    stavOtevreSe = 1;
+    stavZavreSe = 0;
+  }
+  if(spinacOtevreno == HIGH){
+    //prednastavi stav brany
+    stavOtevreSe = 0;
+    stavZavreSe = 1;
+  }
   if (irrecv.decode(&results)){             //
         Serial.println(results.value, DEC); // Precte signal
         irrecv.resume();                    //
@@ -51,7 +63,7 @@ void loop(){
   if (results.value == kodStartu){
     startMotor();
   }
-  if (results.value == kodStopu || digitalRead(10) == HIGH || digitalRead(11) == HIGH) { //10 a 11 jsou spinace na koncich koleje
+  if (results.value == kodStopu || digitalRead(10) == HIGH || digitalRead(11) == HIGH || digitalRead(8) == LOW) { //10 a 11 jsou spinace na koncich koleje
     stopMotor();
   }
 }
